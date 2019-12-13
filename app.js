39e02e64c623bd2c45821474f717f7bc3682cf6b
soulintzis <esoulintzhs@gmail.com>
2019-12-13 23:07:42 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require("express");
+const path = require('path');
+var app = express();
+
+app.set('view engine', 'ejs');
+app.use('/css',express.static(path.join(__dirname, 'css')));
+app.use('/images',express.static(path.join(__dirname, 'images')));
+app.use('/scripts',express.static(path.join(__dirname, 'scripts')));
+app.use('/views',express.static(path.join(__dirname, 'views')));
+
+app.get("/", function(req, res){
+    res.render('login');
+});
+
+app.get("/signup", function(req, res){
+    res.render('signup');
+});
+
+app.listen(80, () => {
+    console.log("The app is listening on port 80.");
+});
