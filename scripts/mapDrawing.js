503a349d1fe628f37e7fe3bd7759d4bdb8cb280c
soulintzis <esoulintzhs@gmail.com>
2019-12-18 01:52:05 +0200

    add map

---
+++ scripts/mapDrawing.js
@@ -0,0 +1,25 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 38.246639, lng: 21.734573 },
+        zoom: 13
+    });
+
+    var drawingManager = new google.maps.drawing.DrawingManager({
+        drawingMode: google.maps.drawing.OverlayType.POLYGON,
+        drawingControl: true,
+        drawingControlOptions: {
+            position: google.maps.ControlPosition.BOTTOM_CENTER,
+            drawingModes: ['polygon', 'rectangle']
+        },
+        markerOptions: { icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png' },
+        circleOptions: {
+            fillColor: '#000000',
+            fillOpacity: 1,
+            strokeWeight: 5,
+            clickable: false,
+            editable: true,
+            zIndex: 1
+        }
+    });
+    drawingManager.setMap(map);
+}
