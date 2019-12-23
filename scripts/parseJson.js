78ae4122d8ca7ab592a27636b1afb32e4c07fe8c
soulintzis <esoulintzhs@gmail.com>
2019-12-23 20:25:21 +0200

    Update parseJson.js

--- scripts/parseJson.js
+++ scripts/parseJson.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const filePath = '../uploads/'
-
+const latitude_center = 38.2466395;
+const longitude_center = 21.753150;
+var counter = 0;
 module.exports = {
     readJsonObjectFromFile: (filename) => {
         path = filePath + filename;
@@ -10,8 +12,37 @@ module.exports = {
             for(item in jsonObj){
                 for(subItem in jsonObj[item]){
                     location = jsonObj[item][subItem];
+                    lat = location.latitudeE7/10000000;
+                    lon = location.longitudeE7/10000000;
+                    counter = counter + 1;
+                    if(module.exports.checkLocation(lat, lon) > 10.0) {
+                        console.log(lat, lon + ' outside');
+                    }else{
+                        console.log(lat, lon + ' inside');
+                    }
                 }
             }
+            console.log(counter);
         });
+    },
+    //This function takes in latitude and longitude of two location and returns the distance between them as the crow flies (in km)
+    checkLocation: (latp, lonp) => {
+        const R = 6371; // Radius of the earth in km
+        const distance_latitude = module.exports.degrees_to_radians(latp - latitude_center);
+        const distance_longitude = module.exports.degrees_to_radians(lonp - longitude_center);
+        latp = module.exports.degrees_to_radians(latp);
+        lonp = module.exports.degrees_to_radians(lonp);
+
+        const a = Math.sin(distance_latitude/2) * Math.sin(distance_latitude/2) +
+        Math.sin(distance_longitude/2) * Math.sin(distance_longitude/2) * Math.cos(latitude_center) * Math.cos(latp); 
+                
+        const b = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+        const result = R * b;
+        return result;
+    },
+
+    degrees_to_radians: (degrees) => {
+        var pi = Math.PI;
+        return degrees * (pi/180);
     }
 };
