0d2dd8dd859fe76746fc2a80cd0391d3ca826e29
dimitrisioan <dimitrisioan@users.noreply.github.com>
2020-02-09 18:33:59 +0200

    ...

---
+++ scripts/utils.js
@@ -0,0 +1,9 @@
+function openSlideMenu(){
+    document.getElementById('side-menu').style.width = '250px';
+    document.getElementById('main').style.marginLeft = '250px';
+  }
+
+  function closeSlideMenu(){
+    document.getElementById('side-menu').style.width = '0';
+    document.getElementById('main').style.marginLeft = '0';
+  }
