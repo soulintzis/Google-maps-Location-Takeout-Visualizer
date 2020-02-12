17bda55a7b19141e416e8b23e0774e58fca54d64
dimitrisioan <dimitrisioan@users.noreply.github.com>
2020-02-12 22:23:38 +0200

    new Home UI boy

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,9 +1,19 @@
-function openSlideMenu(){
-    document.getElementById('side-menu').style.width = '250px';
-    document.getElementById('main').style.marginLeft = '250px';
-  }
+let width = window.matchMedia("(max-width: 600px)");
 
-  function closeSlideMenu(){
-    document.getElementById('side-menu').style.width = '0';
-    document.getElementById('main').style.marginLeft = '0';
+function openNav() {
+  if(width.matches) {
+    document.getElementById("mySidebar").style.width = "40vw";
+    document.getElementById("nav_btn").style.display = "none";
+
+  }else{
+    document.getElementById("mySidebar").style.width = "20vw";
+    document.getElementById("nav_btn").style.display = "none";
   }
+  //document.getElementById("main").style.marginLeft = "40vw";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+  document.getElementById("nav_btn").style.display = "block";
+}
