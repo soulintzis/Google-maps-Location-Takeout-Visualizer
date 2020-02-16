779722df6698e7ea454ad671554caeeaf6bf17c0
soulintzis <esoulintzhs@gmail.com>
2020-02-16 04:51:17 +0200

    fix css and add leaderboard

--- scripts/user_info.js
+++ scripts/user_info.js
@@ -1,15 +1,8 @@
 getMinTimestamp();
 getMaxTimestamp();
 
-async function getUser() {
-	const response = await fetch("http://localhost:3000/api/current_user");
-	return await response.json();
-}
-
 async function getMinTimestamp() {
-    const user = await getUser();
-	const user_id = user.user_id;
-    const url = 'http://localhost:3000/api/location/min_timestamp/' + user_id;
+    const url = 'http://localhost:3000/api/min_date';
     let xhr = new XMLHttpRequest;
     xhr.open('GET', url, true)
 
@@ -18,9 +11,8 @@ async function getMinTimestamp() {
         if (this.status === 200)
         {
             let data = JSON.parse(this.response)
-            let from = new Date(data[0].timestampMs).toLocaleDateString("en-US")
             output = '';
-            output += '<span>' + from + '</span>';
+            output += '<span>' + data + '</span>';
             document.getElementById('timestamp_from').innerHTML = output;
         }else {
             output = '';
@@ -33,9 +25,7 @@ async function getMinTimestamp() {
 }
 
 async function getMaxTimestamp() {
-    const user = await getUser();
-	const user_id = user.user_id;
-    const url = 'http://localhost:3000/api/location/max_timestamp/' + user_id;
+    const url = 'http://localhost:3000/api/max_date';
     let xhr = new XMLHttpRequest;
     xhr.open('GET', url, true)
 
@@ -44,10 +34,47 @@ async function getMaxTimestamp() {
         if (this.status === 200)
         {
             let data = JSON.parse(this.response)
-            let until = new Date(data[0].timestampMs).toLocaleDateString("en-US")
             output = '';
-            output += '<span>' + until + '</span>';
+            output += '<span>' + data + '</span>';
             document.getElementById('timestamp_until').innerHTML = output;
+        }else {
+            console.log('error')
+        }
+    }
+
+    xhr.send()
+}
+getLeaderBoard()
+async function getLeaderBoard() {
+    const url = 'http://localhost:3000/api/leaderboard';
+    let xhr = new XMLHttpRequest;
+    xhr.open('GET', url, true)
+
+    xhr.onload = function () {
+
+        if (this.status === 200)
+        {
+            let data = JSON.parse(this.response)
+            console.log(data);
+            output = '<table>' +
+                        '<tr>' +
+                            '<th>' + 'Ranking' + '</th>' +
+                            '<th>' + 'Short Username' + '</th>'+
+                            '<th>' + 'Eco-score' + '</th>'
+                        '</tr>';
+            for(let i = 1; i <= data.length; i++) {
+                    output +=  '<tr>' +
+                                    '<td>' + i + '. ' + '</td>' 
+                                + '<td>' + data[i-1].username.substr(0, 5) + '...' + '</td>' 
+                                + '<td>' + data[i-1].eco_score  + '</td>' 
+                                + '</tr>';
+                    if(i > 3) {
+                        break;
+                    }
+            }
+            output += '</table>'
+            console.log(output)
+            document.getElementById('leaderBoard').innerHTML = output;
         }else {
             console.log('error')
         }
