6db15cbab36811cd377ea5269a3ac0c8f9788899
soulintzis <esoulintzhs@gmail.com>
2020-02-18 17:24:38 +0200

    database url

--- config/db.js
+++ config/db.js
@@ -1,4 +1,3 @@
 module.exports = {
-    //  dbUrl: 'mongodb+srv://admin:e74d42x75@webapp-vuzaa.gcp.mongodb.net/crowdsourcing_app?retryWrites=true&w=majority'
-     dbUrl: "mongodb://localhost:27017/crowdsourcing_app"
+     dbUrl: //database url goes here
 }
